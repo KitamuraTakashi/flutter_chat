69d580ecb862eb6c52581f072ee8275a5571e4c5
takashi.kitamura <takashi.kitamura0426@gmail.com>
2020-06-13 16:38:41 +0900

    Add:dartの静的解析ツールを導入

--- lib/view/screens/pages/login_page.dart
+++ lib/view/screens/pages/login_page.dart
@@ -18,16 +18,16 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('ログイン'),
+          title: const Text('ログイン'),
         ),
         body: Center(
             child: Container(
-          padding: EdgeInsets.all(24),
+          padding: const EdgeInsets.all(24),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               TextFormField(
-                decoration: InputDecoration(labelText: 'メールアドレス'),
+                decoration: const InputDecoration(labelText: 'メールアドレス'),
                 onChanged: (String value) {
                   setState(() {
                     email = value;
@@ -35,7 +35,7 @@ class _LoginPageState extends State<LoginPage> {
                 },
               ),
               TextFormField(
-                decoration: InputDecoration(labelText: 'パスワード'),
+                decoration: const InputDecoration(labelText: 'パスワード'),
                 onChanged: (String value) {
                   setState(() {
                     password = value;
@@ -43,7 +43,7 @@ class _LoginPageState extends State<LoginPage> {
                 },
               ),
               Container(
-                padding: EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
                 child: Text(infoText),
               ),
               Container(
@@ -51,26 +51,26 @@ class _LoginPageState extends State<LoginPage> {
                 child: RaisedButton(
                     color: Colors.blue,
                     textColor: Colors.white,
-                    child: Text('ユーザ登録'),
+                    child: const Text('ユーザ登録'),
                     onPressed: () async {
                       try {
                         //メールアドレス・パスワードでユーザ登録
-                        final FirebaseAuth auth = FirebaseAuth.instance;
-                        final AuthResult result =
+                        final auth = FirebaseAuth.instance;
+                        final result =
                             await auth.createUserWithEmailAndPassword(
                                 email: email, password: password);
 
-                        final FirebaseUser user = result.user;
+                        final user = result.user;
 
                         //ユーザ登録に成功した場合
                         await Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(builder: (context) {
+                          MaterialPageRoute<void>(builder: (context) {
                             return ChatPage(user: user);
                           }),
                         );
-                      } catch (error) {
+                      } on Exception catch (error) {
                         setState(() {
-                          infoText = "ログインに失敗しました。:${error.message}";
+                          infoText = 'ログインに失敗しました。:$error';
                         });
                       }
                     }),
@@ -79,25 +79,24 @@ class _LoginPageState extends State<LoginPage> {
                 width: double.infinity,
                 child: OutlineButton(
                   textColor: Colors.blue,
-                  child: Text('ログイン'),
+                  child: const Text('ログイン'),
                   onPressed: () async {
                     try {
                       //メール・パスワードでログイン
-                      final FirebaseAuth auth = FirebaseAuth.instance;
-                      final AuthResult result =
-                          await auth.signInWithEmailAndPassword(
-                              email: email, password: password);
+                      final auth = FirebaseAuth.instance;
+                      final result = await auth.signInWithEmailAndPassword(
+                          email: email, password: password);
                       //ログインに成功した時の処理
-                      final FirebaseUser user = result.user;
+                      final user = result.user;
                       await Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(builder: (context) {
+                          MaterialPageRoute<void>(builder: (context) {
                         return ChatPage(
                           user: user,
                         );
                       }));
-                    } catch (error) {
+                    } on Exception catch (error) {
                       setState(() {
-                        infoText = "ログインに失敗しました${error.message}";
+                        infoText = 'ログインに失敗しました$error.message';
                       });
                     }
                   },
