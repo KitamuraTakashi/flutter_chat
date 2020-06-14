24676cb60b1bc18ba36536b47ef082b2c835a80b
takashi.kitamura <takashi.kitamura0426@gmail.com>
2020-06-14 11:33:49 +0900

    Add:ログアウト機能を実装

--- lib/view/screens/pages/chat_page.dart
+++ lib/view/screens/pages/chat_page.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterchat/view/screens/pages/login_page.dart';
 
 class ChatPage extends StatelessWidget {
-  ChatPage({this.user});
+  const ChatPage({this.user});
   // ユーザー情報
   final FirebaseUser user;
 
@@ -10,7 +11,25 @@ class ChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('チャット'),
+        title: const Text('チャット'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () async {
+              /*
+              ログアウト処理
+              内部で保持しているログイン情報などが初期化される
+              */
+              await FirebaseAuth.instance.signOut();
+              //ログイン画面に遷移＋チャット画面を破棄
+              await Navigator.of(context).pushReplacement(
+                MaterialPageRoute<void>(builder: (context) {
+                  return LoginPage();
+                }),
+              );
+            },
+          )
+        ],
       ),
       body: Center(
         // ユーザー情報を表示
